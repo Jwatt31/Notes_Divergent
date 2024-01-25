93027c333e6674b1f952d28e91493ea34150189c
James Whatley <jamesww189@gmail.com>
2024-01-24 20:02:15 -0600

    added some problems to haker rank need to practice

--- Python-Hakerank/Haker_Rank problems.py
+++ Python-Hakerank/Haker_Rank problems.py
@@ -1,1 +1,22 @@
 
+# create a function that adds the sum of the 2 proceeding number to the given number of time 
+
+# create a main function
+
+# Create a function with a for i loop 
+
+# make a function that adds the sum of the number proccceding it in the array
+
+n = 10
+def fib(n):
+    fib = [0,1]
+    for i in range (n):
+        next_num = fib[-1] + fib[-2]
+        if next_num < n:
+            fib.append(next_num)
+        else:
+            break
+    return fib
+
+re = fib(n)
+print(re)
