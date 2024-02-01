fe69ee2829e8debaa00774b08c1d776f42be88c6
James Whatley <jamesww189@gmail.com>
2024-02-01 11:43:36 -0600

    EOD heading to detal shortly

--- Django/4django/md_project/wsgi.py
+++ Django/4django/md_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "md_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mb_project.settings")
 
 application = get_wsgi_application()
