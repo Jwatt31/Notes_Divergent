5ab7e0b72128efb90142fbda707991bce1607191
James Whatley <jamesww189@gmail.com>
2024-02-02 16:39:48 -0600

    wooooo friday almost 6

--- Django/5django/blog_project/blog/views.py
+++ Django/5django/blog_project/blog/views.py
@@ -1,5 +1,5 @@
-# blog/views.py
-from django.views.generic import ListView
+# Blogs/view
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here to populate data from database
@@ -8,3 +8,9 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+
+# We are using Django's views.generic API by importing DetailView Class
+# Then we are makeing this DetailView Class our own with BlogDetailView
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
