5daa79a75642bead629df0c01bf0ba30a3af6b91
James Whatley <jamesww189@gmail.com>
2024-02-02 17:18:12 -0600

    getting to end of day

--- Django/5django/blog_project/blog/urls.py
+++ Django/5django/blog_project/blog/urls.py
@@ -1,9 +1,11 @@
 # blog/urls.py
 from django.urls import path
 
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView, BlogCreateView
 # By using the period .views we reference the current directory, which is our pages app containing both views.py and urls.py
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
+    path('post/<int:pk>/',BlogDetailView.as_view(), name='post_detail'),
+    path("post_new", BlogCreateView)
 ]
