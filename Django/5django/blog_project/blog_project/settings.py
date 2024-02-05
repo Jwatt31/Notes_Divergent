b424e2e31502d0d8ff690f5322387d0838ce5a5f
James Whatley <jamesww189@gmail.com>
2024-02-05 12:45:23 -0600

    getting to end of day

--- Django/5django/blog_project/blog_project/settings.py
+++ Django/5django/blog_project/blog_project/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-LOGOUT_REDIRECT_URL = 'home' # need this to tell reverse_ to retuen to the ho,e page with the changes made
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -127,3 +127,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Logout stuff
+LOGOUT_REDIRECT_URL = 'home' # need this to tell reverse_ to retuen to the ho,e page with the changes made
