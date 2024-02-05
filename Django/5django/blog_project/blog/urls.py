279deff336e0d88ed038aef443abcdb2fcd5c357
James Whatley <jamesww189@gmail.com>
2024-02-05 10:55:03 -0600

    ll

--- Django/5django/blog_project/blog/urls.py
+++ Django/5django/blog_project/blog/urls.py
@@ -1,11 +1,11 @@
 # blog/urls.py
 from django.urls import path
-
-from .views import BlogListView, BlogDetailView, BlogCreateView
-# By using the period .views we reference the current directory, which is our pages app containing both views.py and urls.py
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='home'),
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/',BlogDetailView.as_view(), name='post_detail'),
-    path("post_new", BlogCreateView)
+    path('',BlogListView.as_view(), name='home')
 ]
--- Django/5django/blog_project/blog/views.py
+++ Django/5django/blog_project/blog/views.py
@@ -1,13 +1,30 @@
 # Blogs/view
-# blog/urls.py
-from django.urls import path
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-from .views import BlogListView, BlogDetailView, BlogCreatelView
-# By using the period .views we reference the current directory, which is our pages app containing both views.py and urls.py
 
-urlpatterns = [
-    path('', BlogListView.as_view(), name='home'),
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), 
-    # pk and id are interchangeable
-    path('post/new/', BlogCreatelView.as_view(), name='post_new'),
-]
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView): #Show details of a Post in it's own page
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home') # It won't execute the URL redirect until our view has finished deleting the blog post
