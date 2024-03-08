89edf98047dbb5914fac933502d46354a5dd7aa6
James Whatley <jamesww189@gmail.com>
2024-03-08 13:01:12 -0600

    uh

--- script.ps1
+++ script.ps1
@@ -22,5 +22,36 @@ if(($args.Count -ne 1) -or ($args[0] -eq 'help')){
 	exit;
 }else{
 	# Do the actual script actions here.
+    switch ($args[0]) {
+        "printdate" {
+
+            $utcTime = (Get-Date).ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss zzz: ($localTimeZone)")
+            $date24WeeksAgo = (Get-Date).AddDays(-168)
+            $localTime = Get-Date
+            $localTimeZone = (Get-TimeZone).Id
+            
+
+            Write-Host "Local Time: $($localTime.ToString('yyyy-MM-dd HH:mm:ss zzz')) ($localTimeZone)"
+            Write-Host "Zulu Time : $utcTime"
+            Write-Host "24 Weeks  : $($date24WeeksAgo.ToString('yyyy-MM-dd HH:mm:ss zzz')) ($localTimeZone)"
+
+            Write-Host ("Is today " + (Get-Date).IsDaylightSavingTime());
+            Write-Host ("Is 24w ago "+ (Get-Date).AddDays(-168).IsDaylightSavingTime());
+
+        }
+        "printhello" { Write-Host "hello world, it's a me" }
+        "getdays" {
+            if($args.Count -lt 2){
+                $getdays =30
+            }else{
+                $getdays = $args[1];
+            }
+            for ($i=0; $i -lt $getdays; $i++)
+            {
+                write
+            }
+        }
+        Default { }
+    }
 	exit;
 };
