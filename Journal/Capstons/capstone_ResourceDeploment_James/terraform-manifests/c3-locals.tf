ee2269e44e369018d957153140e5de44a0398114
James Whatley <jamesww189@gmail.com>
2024-03-13 11:25:09 -0500

    as

--- Journal/Capstons/capstone_ResourceDeploment_James/terraform-manifests/c3-locals.tf
+++ Journal/Capstons/capstone_ResourceDeploment_James/terraform-manifests/c3-locals.tf
@@ -1,8 +1,8 @@
 # Define Local Values in Terraform
 locals {
-  owners = var.business_divsion
+  owners = var.business_division
   environment = var.environment
-  resource_name_prefix = "${var.business_divsion}-${var.environment}"
+  resource_name_prefix = "${var.business_division}-${var.environment}"
   #name = "${local.owners}-${local.environment}"
   common_tags = {
     owners = local.owners
