304af9c006ef551c7b59a7ee3e034543a40b42a9
James Whatley <jamesww189@gmail.com>
2024-03-13 15:07:26 -0500

    okay fixed some name changes

--- Journal/Capstons/capstone_ResourceDeploment_James/terraform-manifests/c6-01-vnet-input-variables.tf
+++ Journal/Capstons/capstone_ResourceDeploment_James/terraform-manifests/c6-01-vnet-input-variables.tf
@@ -1,12 +1,12 @@
 # Virtual Network, Subnets and Subnet NSG's
 
 ## Virtual Network
-variable "vnet_name" {
+variable "vnet_testname" {
   description = "Virtual Network name"
   type = string
   default = "vnet-default"
 }
-variable "vnet_address_space" {
+variable "vnet_address_testspace" {
   description = "Virtual Network address_space"
   type = list(string)
   default = ["10.0.0.0/16"]
@@ -14,13 +14,13 @@ variable "vnet_address_space" {
 
 
 # Web Subnet Name
-variable "web_subnet_name" {
+variable "web_subnet_testname" {
   description = "Virtual Network Web Subnet Name"
   type = string
   default = "websubnet"
 }
 # Web Subnet Address Space
-variable "web_subnet_address" {
+variable "web_subnet_testaddress" {
   description = "Virtual Network Web Subnet Address Spaces"
   type = list(string)
   default = ["10.0.1.0/24"]
--- Journal/Capstons/capstone_ResourceDeploment_James/terraform-manifests/c6-03-web-subnet-and-nsg.tf
+++ Journal/Capstons/capstone_ResourceDeploment_James/terraform-manifests/c6-03-web-subnet-and-nsg.tf
@@ -1,9 +1,9 @@
 # Resource-1: Create WebTier Subnet
 resource "azurerm_subnet" "websubnet" {
-  name                 = "${azurerm_virtual_network.vnet.name}-${var.web_subnet_name}"
+  name                 = "${azurerm_virtual_network.vnet.name}-${var.web_subnet_testname}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = var.web_subnet_address  
+  address_prefixes     = var.web_subnet_testaddress  
 }
 
 # Resource-2: Create Network Security Group (NSG)
